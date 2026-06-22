f6595f1818a87e9ee65c35324a51249f2f418689
hassammd <115186847+hassammd@users.noreply.github.com>
2026-06-23 00:12:12 +0500

    store data in local storage

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,91 +1,116 @@
-import { createContext, useContext, useState } from "react";
-import fajar from "../assets/fajar.png";
+import { createContext, useContext, useEffect, useState } from "react";
+// import fajar from "../assets/fajar.png";
 import zuhr from "../assets/zuhr.png";
 import Asr from "../assets/Asr.png";
 import maghrib from "../assets/maghrib.png";
 import isha from "../assets/isha.png";
-import { FaJar } from "react-icons/fa6";
+import fajar from "../assets/fajar.png";
 import star from "../assets/star.png";
 import quran from "../assets/quran.png";
 //create
 const myContext = createContext();
-
+//Default Data Template
+const defaultPrayers = [
+  {
+    id: 1,
+    name: "Fajar",
+    time: "04:15 AM",
+    icon: fajar,
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Zuhr",
+    time: "12:30 AM",
+    icon: zuhr,
+    completed: false,
+  },
+  {
+    id: 3,
+    name: "Asr",
+    time: "04:45 PM",
+    icon: Asr,
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Maghrib",
+    time: "07:15 PM",
+    icon: maghrib,
+    completed: false,
+  },
+  {
+    id: 5,
+    name: "Isha",
+    time: "08:45 PM",
+    icon: isha,
+    completed: false,
+  },
+];
+const defaultGoodDeeds = [
+  {
+    id: 1,
+    name: "Fajar",
+    time: "04:15 AM",
+    icon: fajar,
+    completed: false,
+  },
+  {
+    id: 2,
+    name: "Zuhr",
+    time: "12:30 AM",
+    icon: zuhr,
+    completed: false,
+  },
+  {
+    id: 3,
+    name: "Asr",
+    time: "04:45 PM",
+    icon: Asr,
+    completed: false,
+  },
+  {
+    id: 4,
+    name: "Maghrib",
+    time: "07:15 PM",
+    icon: maghrib,
+    completed: false,
+  },
+  {
+    id: 5,
+    name: "Isha",
+    time: "08:45 PM",
+    icon: isha,
+    completed: false,
+  },
+];
 //provider
 const MyContextProvider = ({ children }) => {
-  const [prayers, setPrayers] = useState([
-    {
-      id: 1,
-      name: "Fajar",
-      time: "04:15 AM",
-      icon: FaJar,
-      completed: false,
-    },
-    {
-      id: 2,
-      name: "Zuhr",
-      time: "12:30 AM",
-      icon: zuhr,
-      completed: false,
-    },
-    {
-      id: 3,
-      name: "Asr",
-      time: "04:45 PM",
-      icon: Asr,
-      completed: false,
-    },
-    {
-      id: 4,
-      name: "Maghrib",
-      time: "07:15 PM",
-      icon: maghrib,
-      completed: false,
-    },
-    {
-      id: 5,
-      name: "Isha",
-      time: "08:45 PM",
-      icon: isha,
-      completed: false,
-    },
-  ]);
-  const [goodDeeds, setGoodDeeds] = useState([
-    {
-      id: 1,
-      name: "Quran Tilawat",
+  const [prayers, setPrayers] = useState(() => {
+    const savedPrayers = localStorage.getItem("prayersData");
+    if (savedPrayers) {
+      return JSON.parse(savedPrayers);
+    }
+    return defaultPrayers;
+  });
+  const [goodDeeds, setGoodDeeds] = useState(() => {
+    const savedGoodDeeds = localStorage.getItem("GoodDeedsData");
+    if (savedGoodDeeds) {
+      return JSON.parse(savedGoodDeeds);
+    }
+    return defaultGoodDeeds;
+  });
 
-      icon: quran,
-      completed: false,
-    },
-    {
-      id: 2,
-      name: "Sadqa",
+  //save state for prayers
+  useEffect(() => {
+    localStorage.setItem("prayersData", JSON.stringify(prayers));
+  }, [prayers]);
 
-      icon: quran,
-      completed: false,
-    },
-    {
-      id: 3,
-      name: "Zikr",
+  //save state for good deeds
 
-      icon: quran,
-      completed: false,
-    },
-    {
-      id: 4,
-      name: "Quran Tilawat",
-
-      icon: quran,
-      completed: false,
-    },
-    {
-      id: 5,
-      name: "Quran Tilawat",
-
-      icon: quran,
-      completed: false,
-    },
-  ]);
+  useEffect(() => {
+    localStorage.setItem("GoodDeedsData", JSON.stringify(goodDeeds));
+  }, [goodDeeds]);
 
   //prayers
   const prayersCount = prayers.filter((prayer) => prayer.completed).length;
